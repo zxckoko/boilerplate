ae8a2323e31bd482c6252bfbe3fa087241747139
DefaultLGTM <10623125+DefaultLGTM@users.noreply.github.com>
2025-07-18 01:28:26 +0800

    Update users table
    Update schema and added new seed content

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+//         User::factory(1000)->create();
 
-        User::factory()->create([
+         $users = User::all();
+
+         foreach ($users as $user) {
+             $user->address_1 = fake()->address();
+             $user->address_2 = rand(1, 100) > 88 ? fake()->address() : null;
+             $user->save();
+         }
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
