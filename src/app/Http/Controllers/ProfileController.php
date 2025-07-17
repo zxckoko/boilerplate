e7be577ccd75ff4eae36a2e23e9cf10a6c2454a4
DefaultLGTM <10623125+DefaultLGTM@users.noreply.github.com>
2025-07-18 01:28:55 +0800

    Link Users data from backend to frontend via Inertia

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,6 +14,15 @@ use Inertia\Response;
 
 class ProfileController extends Controller
 {
+    public function index()
+    {
+        $users = User::all();
+
+        return Inertia::render('Dashboard', [
+            'users' => $users,
+            'usersCount' => count($users),
+        ]);
+    }
     /**
      * Display the user's profile form.
      */
