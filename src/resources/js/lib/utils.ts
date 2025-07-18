882ce4ec31400d0f258d48319896db07ca044c65
DefaultLGTM <10623125+DefaultLGTM@users.noreply.github.com>
2025-07-18 16:59:16 +0800

    Added reference to util class for tailwind

--- src/resources/js/lib/utils.ts
+++ src/resources/js/lib/utils.ts
@@ -1,3 +1,4 @@
+// Reference: https://ayberkyavas.com/blogs/wtf-is-clsx-twmerge-cn-in-tailwindcss (Check this out)
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
