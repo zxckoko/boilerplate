970d2bc2a33b2c77dcb733acb8402dbb9bc0048f
DefaultLGTM <10623125+DefaultLGTM@users.noreply.github.com>
2025-07-31 19:05:52 +0800

    Add auth checks to custom package dashboards

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,34 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        $this->configureAuthToCustomPackges();
+    }
+
+    private function configureAuthToCustomPackges()
+    {
+        Gate::define('viewPulse', function () {
+            return request()->user() && request()->user()->hasRole('Administrator');
+        });
+
+        Gate::define('viewHorizon', function () {
+            return request()->user() && request()->user()->hasRole('Administrator');
+        });
+
+        Gate::define('viewTelescope', function () {
+            return request()->user() && request()->user()->hasRole('Administrator');
+
+        });
+
+        /*
+         * INSECURE: https://log-viewer.opcodes.io/docs/3.x/configuration/access-to-log-viewer
+         * - tried all implementations decsribed in the document above
+         * - Gate, Auth, Spatie custom middleware
+         * - EITHER Removal of "\Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,"
+         * - OR removal of Gate (any) check will only work... which makes access insecure
+         * */
+        // Gate::define('viewLogViewer', function () {
+        //     return request()->user() && request()->user()->hasRole('Administrator');
+        // });
     }
 }
